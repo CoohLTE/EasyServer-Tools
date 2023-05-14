4d709523880017c3032dac0d4cb96ae6199416e7
CoohLTE <saiqvo495@gmail.com>
2023-05-14 15:34:15 -0300

    v1.0.0-FIX

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-x="exitmenu"
-
 apt update -qq
 apt upgrade -y -qq
 clear
@@ -11,33 +9,30 @@ sleep 3
 apt install -qq wget curl figlet net-tools -y
 
 menu() {
-    while true $x != "exitmenu"
-    do
-        clear
-        figlet -c EasyServer Tools
-        echo ""
-        echo ""
-        echo "Criado Pela CoohLTE"
-        echo ""
-        PS3="Escolha Uma Opcao: "
-        cal=("Servidores Fisicos" "Sair")
-        select x in "${cal[@]}"; do
-            case $x in
-                "Servidores Fisicos")
-                    clear
-                    bash <(curl -s http://storage.cchcloud.tk/r/menuFisico.sh)
-                ;;
-                "Sair")
-                    echo "Saindo..."
-                    sleep 2
-                    clear
-                    exit
-                ;;
-                *)
-                    echo "Opcao Invalida!"
-                ;;
-            esac
-        done
+    clear
+    figlet -c EasyServer Tools
+    echo ""
+    echo ""
+    echo "Criado Pela CoohLTE"
+    echo ""
+    PS3="Escolha Uma Opcao: "
+    cal=("Servidores Fisicos" "Sair")
+    select x in "${cal[@]}"; do
+        case $x in
+        "Servidores Fisicos")
+            clear
+            bash <(curl -s http://storage.cchcloud.tk/r/menuFisico.sh)
+            ;;
+        "Sair")
+            echo "Saindo..."
+            sleep 2
+            clear
+            exit
+            ;;
+        *)
+            echo "Opcao Invalida!"
+            ;;
+        esac
     done
 }
 
